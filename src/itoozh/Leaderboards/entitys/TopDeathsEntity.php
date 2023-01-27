a8d903d82453f058f8396a2be7495a40eacdd571
artsvn <121850372+artsvn@users.noreply.github.com>
2023-01-28 00:28:20 +0800

    Update TopDeathsEntity.php

--- src/itoozh/Leaderboards/entitys/TopDeathsEntity.php
+++ src/itoozh/Leaderboards/entitys/TopDeathsEntity.php
@@ -70,16 +70,16 @@ class TopDeathsEntity extends Human
      * @return bool
      */
     public function onUpdate(int $currentTick): bool{
-        $line = "–––––x–––––x–––––";
+        $line = "–––––x–-----––––x–––––";
         $msg = $line . TextFormat::EOL;
-        $msg .= TextFormat::BOLD . TextFormat::WHITE . "TOP DEATHS" . TextFormat::EOL;
+        $msg .= TextFormat::BOLD . TextFormat::WHITE . "KITSCORE BOARD" . TextFormat::EOL;
         $msg .= TextFormat::EOL;
         $place = 1;
         $kills = Leaderboards::getDeathsAsRaw();
         arsort($kills);
         foreach($kills as $player => $kill){
             if($place > 5) break;
-            $msg .= TextFormat::GRAY . $place . ". " . TextFormat::YELLOW . $player . " " . TextFormat::GRAY . "» " . TextFormat::RED . $kill . TextFormat::EOL;
+            $msg .= TextFormat::GRAY . $place . ". " . TextFormat::YELLOW . $player . " " . TextFormat::GRAY . "» " . TextFormat::RED . $kill . TextFormat::RED . " deaths" . TextFormat::EOL;
             $place++;
         }
         $msg .= $line;
